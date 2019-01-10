87203aa1ae9b343f1db326e32b55f2be676b8b5f
Plawan Rath <plawanrath@gmail.com>
2019-01-09 21:55:01 -0500

    update meeting room problem

--- MeetingRooms_253.java
+++ MeetingRooms_253.java
@@ -11,30 +11,29 @@ public class Interval {
 
 class Solution {
     public int minMeetingRooms(Interval[] intervals) {
-        if(intervals.length <= 1) {
+        if(intervals.length <= 1)
             return intervals.length;
-        }
-        Arrays.sort(intervals, new IntervalComparator());
-        PriorityQueue<Interval> pq = new PriorityQueue<Interval>(new IntervalEndComparator());
+        Arrays.sort(intervals, new Comparator<Interval>() {
+           @Override
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        
+        PriorityQueue<Interval> pq = new PriorityQueue<Interval>(new Comparator<Interval>() {
+           @Override
+            public int compare(Interval a, Interval b) {
+                return a.end - b.end;
+            }
+        });
+        
         pq.add(intervals[0]);
         for(int i=1;i<intervals.length;i++) {
-            if(intervals[i].start >= pq.peek().end) {
+            Interval val = pq.peek();
+            if(intervals[i].start >= val.end)
                 pq.poll();
-            } 
             pq.add(intervals[i]);
         }
         return pq.size();
-    }
-}
-
-class IntervalComparator implements Comparator<Interval> {
-    public int compare(Interval a, Interval b) {
-        return a.start - b.start;
-    }
-}
-
-class IntervalEndComparator implements Comparator<Interval> {
-    public int compare(Interval a, Interval b) {
-        return b.end - a.end;
     }
 }
